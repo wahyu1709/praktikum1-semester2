bae4ecf67ac324ef1830eaf095fc39ed548cd54c
wahyu1709 <44396432+wahyu1709@users.noreply.github.com>
2021-03-14 21:50:16 +0700

    penambahan push dan validasi

--- array_siswa.php
+++ array_siswa.php
@@ -1,9 +1,34 @@
 <?php
+    error_reporting(0);
     $nama_siswa = $_POST['nama'];
     $mata_kuliah = $_POST['matkul'];
     $nilai_uts = $_POST['nilai_uts'];
     $nilai_uas = $_POST['nilai_uas'];
     $nilai_tugas = $_POST['nilai_tugas'];
+
+    if(isset($_POST['simpan'])){
+        if(empty($nama_siswa)){
+            echo "<p style='color:#F00;'>* Masukan Nama Anda</p>";
+        }
+        elseif(empty($nilai_uts)){
+            echo "<p style='color:#F00;'>* Masukan Nilai UTS Anda</p>";
+        }
+        elseif(empty($nilai_uas)){
+            echo "<p style='color:#F00;'>* Masukan Nilai UAS Anda</p>";
+        }
+        elseif(empty($nilai_tugas)){
+            echo "<p style='color:#F00;'>* Masukan Nilai Tugas Anda</p>";
+        }else{
+            $list1 = ['nama'=>'Dimas','matkul'=>'DDP','uts'=>90,'uas'=>85,'tugas'=>80];
+            $list2 = ['nama'=>'Yusuf','matkul'=>'BDI','uts'=>85,'uas'=>90,'tugas'=>80];
+            $list3 = ['nama'=>$nama_siswa,'matkul'=>$mata_kuliah,'uts'=>$nilai_uts,'uas'=>$nilai_uas,'tugas'=>$nilai_tugas];
+            $array_siswa = [$list1,$list2,$list3];
+            array_push($list3,$nama_siswa,$mata_kuliah,$nilai_uts,$nilai_uas,$nilai_uas);
+        }
+    };
+
+
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,14 +72,14 @@
                     <tbody>
                         <?php
                             $nomor = 1;
-                            foreach($nama_siswa as $ns => $k){
+                            foreach($array_siswa as $ns){
                                 echo '<tr><td>'.$nomor.'</td>';
-                                echo '<td>'.$k.'</td>';
-                                echo '<td>'.$mata_kuliah[$ns].'</td>';
-                                echo '<td>'.$nilai_uts[$ns].'</td>';
-                                echo '<td>'.$nilai_uas[$ns].'</td>';
-                                echo '<td>'.$nilai_tugas[$ns].'</td>';
-                                $nilai_akhir = (30*$nilai_uts[$ns]/100) + (30*$nilai_uas[$ns]/100) + (35*$nilai_tugas[$ns]/100);
+                                echo '<td>'.$ns['nama'].'</td>';
+                                echo '<td>'.$ns['matkul'].'</td>';
+                                echo '<td>'.$ns['uts'].'</td>';
+                                echo '<td>'.$ns['uas'].'</td>';
+                                echo '<td>'.$ns['tugas'].'</td>';
+                                $nilai_akhir = (30*$ns['uts']/100) + (30*$ns['uas']/100) + (35*$ns['tugas']/100);
                                 echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
                                 echo '<tr/>';
                                 $nomor++;
